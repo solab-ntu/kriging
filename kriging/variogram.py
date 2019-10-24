731988dcbacc482c14d8ed2ac27df49a2a948fb0
yc.huang71 <yc.huang71@gmail.com>
2019-10-25 00:50:44 +0800

    modified:   kriging/variogram.py

--- kriging/variogram.py
+++ kriging/variogram.py
@@ -1,8 +1,11 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.spatial.distance import pdist
 
+import direct
 
-class VariogramFit:
+
+class Variogram:
 
     def __init__(self, xs: np.array, ys: np.array, lb: np.array, ub: np.array):
         self.xs = xs
@@ -10,22 +13,90 @@ class VariogramFit:
         self.lb = lb
         self.ub = ub
         self.normalized_xs = (self.xs - self.lb) / (self.ub - self.lb)
-        self.cloud = VariogramCloud(self.normalized_xs, self.ys)
-        self.experimental = VariogramExperimental(self.cloud)
+        self.cloud = Cloud(self.normalized_xs, self.ys)
+        self.experimental = Experimental(self.cloud)
+
+    def fit(self):
+        pass
+
+    def plot(self):
+        fig = plt.figure(1)
+        ax = fig.add_subplot(111)
+        self.cloud.plot()
+        self.experimental.plot()
+        ax.set_xlabel("$h\ \mathrm{(normalized)}$")
+        ax.set_ylabel("$\gamma(h)$")
+        ax.set_title("Variogram")
+        plt.show()
 
 
-class VariogramCloud:
+class Cloud:
 
     def __init__(self, normalized_xs: np.array, ys: np.array):
-        self.hs = pdist(normalized_xs, metric='euclidean')
-        self.2gammas = pdist(ys, metric='euclidean')**2
-        self.h_max = np.max(self.hs)
+        self.hs = pdist(normalized_xs, metric='euclidean') # normalized distance
+        self.gammas = (pdist(ys, metric='euclidean')**2) / 2.0 # semi variogram
+        self.hs_max = np.max(self.hs)
+
+    def plot(self):
+        fig = plt.gcf()
+        ax = plt.gca()
+        ax.scatter(self.hs, self.gammas, s=1, c="r")
+        
+
+class Experimental:
+
+    def __init__(self, cloud: Cloud, linspace_num: int=50):
+        self.cloud = cloud
+        self.nums = np.linspace(start=0, stop=cloud.hs_max, num=linspace_num, endpoint=True)
+        self.hs = [(i+j)/2.0 for i, j in zip(self.nums[:-1], self.nums[1:])]
+        self.gammas = self._get_gammas()
+
+    def _get_gammas(self):
+        gammas = []
+        for i, j in zip(self.nums[:-1], self.nums[1:]):
+            cloud_filter = np.logical_and(i < self.cloud.hs, self.cloud.hs <= j)
+            count = np.count_nonzero(cloud_filter)
+            gamma = np.nan
+            if count > 0:
+                total = np.sum(self.cloud.gammas[cloud_filter])
+                gamma = total / count
+            gammas.append(gamma)
+        return gammas
+
+    def plot(self):
+        fig = plt.gcf()
+        ax = plt.gca()
+        ax.scatter(self.hs, self.gammas, s=20, facecolors='none', marker="^", edgecolors="b")
 
 
-class VariogramExperimental:
+class Theoretical:
 
-    def __init__(self, cloud: VariogramCloud):
-        section_count = 50
+    def __init__(self, hs: np.array):
+        self.hs = hs
+        self.sigma2 = None
+        self.theta = None
+        self.p = 1.99
+        self.nugget = 1e-12
+        self.gammas = None
+        self.R = None
+    
+    def set_gammas(self):
+        pass
+
+
+class SpatialCorrelationCoefficient:
+
+    @staticmethod
+    def dace_gaussian(hs: np.array, theta: float, p: float) -> np.array:
+        pass
+
+    @staticmethod
+    def matern_linear(hs: np.array, theta: float) -> np.array:
+        pass
+
+    @staticmethod
+    def matern_cubic(hs: np.array, theta: float) -> np.array:
+        pass
 
 
 if __name__ == "__main__":
@@ -37,4 +108,4 @@ if __name__ == "__main__":
     ub = np.array([6])
 
     variogram = Variogram(xs, ys, lb, ub)
-    print(123)
+    variogram.plot()
